cbc08a0033da37aec7356027899de65cdbbfc467
Onu Camelia <onu.camelia@yahoo.com>
2018-03-27 20:00:14 +0300

    upload project

---
+++ DataIntegration_Elena/DocMng/src/main/java/DocMng/MongoDb/Document.java
@@ -0,0 +1,35 @@
+package DocMng.MongoDb;
+
+public class Document {
+	private Integer documentId;
+	private String documentName;
+	
+	public Document(Integer documentId, String documentName){
+	super();
+	this.documentId = documentId;
+	this.documentName = documentName;
+	}
+	
+	public Document() {
+		super();
+		this.documentId = 0;
+		this.documentName = null;
+	}
+
+	public Integer getDocumentId() {
+		return documentId;
+	}
+
+	public void setDocumentId(Integer documentId) {
+		this.documentId = documentId;
+	}
+
+	public String getDocumentName() {
+		return documentName;
+	}
+
+	public void setDocumentName(String documentName) {
+		this.documentName = documentName;
+	}
+
+}
